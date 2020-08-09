80b46ae9fbdcbbb2e63f543bd404a5d6c9d409a5
12827629 <michael.k.malak@gmail.com>
2020-08-09 11:04:42 +0200

    Apply linting fixes

--- models/User.js
+++ models/User.js
@@ -1,24 +1,45 @@
-var mongoose = require('mongoose');
-var uniqueValidator = require('mongoose-unique-validator');
-var crypto = require('crypto');
+const mongoose = require("mongoose");
+//const uniqueValidator = require("mongoose-unique-validator");
+const crypto = require("crypto");
 
-var UserSchema = new mongoose.Schema({
-    username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },    
-    email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
-    bio: String,
-    image: String,
-    hash: String,
-    salt: String
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			lowercase: true,
+			unique: true,
+			required: [true, "can't be blank"],
+			match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+			index: true,
+		},
+		email: {
+			type: String,
+			lowercase: true,
+			unique: true,
+			required: [true, "can't be blank"],
+			match: [/\S+@\S+\.\S+/, "is invalid"],
+			index: true,
+		},
+		bio: String,
+		image: String,
+		hash: String,
+		salt: String,
+	},
+	{timestamps: true},
+);
 
-UserSchema.methods.setPassword = function (password) {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+UserSchema.methods.setPassword = function(password) {
+	this.salt = crypto.randomBytes(16).toString("hex");
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 10_000, 512, "sha512").toString(
+		"hex",
+	);
 };
 
 UserSchema.methods.validPassword = function(password) {
-     var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-     return this.hash === hash;
-    };
+	const hash = crypto.pbkdf2Sync(password, this.salt, 10_000, 512, "sha512").toString(
+		"hex",
+	);
+	return this.hash === hash;
+};
 
-mongoose.model('User', UserSchema);
+mongoose.model("User", UserSchema);
