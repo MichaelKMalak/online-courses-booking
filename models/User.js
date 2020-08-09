d3791f89c87afcf0c8801ad11311f7fd17e942a6
12827629 <michael.k.malak@gmail.com>
2020-08-09 11:14:06 +0200

    Create a method on the user model to generate a JWT

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 //const uniqueValidator = require("mongoose-unique-validator");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const secret= require("../config").secret;
 
 const UserSchema = new mongoose.Schema(
 	{
@@ -40,6 +42,18 @@ UserSchema.methods.validPassword = function(password) {
 		"hex",
 	);
 	return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+    const today = new Date();
+    const exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+    
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+        }, secret);
 };
 
 mongoose.model("User", UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET users listing. */
 router.get(
 	"/",
-	function(req, res, next) {
+	function (req, res) {
 		res.send("respond with a resource");
 	},
 );
